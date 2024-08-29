67fb828a4b0e209f3f61ea5c972c765b25b98fde
Mathew-Dz <79571930+mohaneddz@users.noreply.github.com>
2024-08-29 16:22:57 +0100

    -Added Menu for Mobile Users -Added Media More Queries

---
+++ public/source/landing.js
@@ -0,0 +1,16 @@
+const menubtn = document.querySelector('.header__menu');
+const menu = document.querySelector('.Menusidebar');
+
+menubtn.addEventListener('click', () => {
+	
+    console.log('clicked');
+    
+    menu.classList.toggle('open');
+	menubtn.classList.toggle('active');
+
+	if (menubtn.classList.contains('active')) {
+		menubtn.innerHTML = '<i class="fi fi-rr-cross"></i>';
+	} else {
+		menubtn.innerHTML = '<i class="fi fi-rr-menu-burger"></i>';
+	}
+});
