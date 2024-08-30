7c24731e4e4e557af5c7aa890016d70b27caf2e4
Mathew-Dz <79571930+mohaneddz@users.noreply.github.com>
2024-08-30 10:02:34 +0100

    -Fixed a bug with The Style of the new Reworked Menu Icon

--- public/source/landing.js
+++ public/source/landing.js
@@ -1,18 +1,21 @@
 'use strict';
 
-const menubtn = document.querySelector('.header__menu');
+const menubtn = document.querySelector('.header__menu a i');
 const menu = document.querySelector('.Menusidebar');
 
-menubtn.addEventListener('click', () => {
+menubtn.addEventListener('click', (e) => {
 	
-    console.log('clicked');
-    
+	e.preventDefault(); // Prevent the default anchor behavior
+    const parent = menubtn.parentElement;
+
     menu.classList.toggle('open');
 	menubtn.classList.toggle('active');
 
-	if (menubtn.classList.contains('active')) {
-		menubtn.innerHTML = '<i class="fi fi-rr-cross"></i>';
-	} else {
-		menubtn.innerHTML = '<i class="fi fi-rr-menu-burger"></i>';
-	}
+    if (parent.classList.contains('active')) {
+        parent.classList.remove('active');
+        menubtn.className = 'fi fi-rr-menu-burger';
+    } else {
+        parent.classList.add('active');
+        menubtn.className = 'fi fi-rr-cross';
+    }
 });
